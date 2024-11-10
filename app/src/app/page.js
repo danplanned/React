51a66de981626e7c731494f6efcd423d7756fc6c
danplanned <elihcnad@gmail.com>
2024-11-09 22:34:28 -0800

    learning through page.js, then Post.js is used to learn about components and props

--- app/src/app/page.js
+++ app/src/app/page.js
@@ -1,4 +1,18 @@
-export default function Home() {
-  return <div></div>;
+import Post from "@/components/Post";
 
-  }
+export default function Home() {
+  return (
+    <div>
+      <Post
+        title="Dan is the best"
+        author="Dan"
+        content="Dan is cracked at programming"
+      />
+      <Post
+        title="Dan is the best"
+        author="Dan"
+        content="Dan is cracked at programming"
+      />
+    </div>
+  );
+}
