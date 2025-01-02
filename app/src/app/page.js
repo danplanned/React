dd502d359c228b12c786980a532b8d17bf1dbadb
danplanned <elihcnad@gmail.com>
2025-01-01 23:45:35 -0800

    learned useState and useEffect

--- app/src/app/page.js
+++ app/src/app/page.js
@@ -79,11 +79,65 @@ export default function Home() {
   );
 }
 */
-
+"use client";
 import { Post } from "@/components/Post";
+import { useState, useEffect } from "react";
 
 export default function Home() {
-  const posts = [];
+  const [content, setContent] = useState("");
+  const [author, setAuthor] = useState("");
+  const [title, setTitle] = useState("");
+  const [posts, setPosts] = useState([]);
 
-  return <div></div>;
+  useEffect(()=> {
+    if(content.length >=10) alert("Your content is getting kinda long!");
+  
+  }, [content])
+
+  function addPost() {
+    const newPost = {
+      author: author,
+      content: content,
+      title: title,
+      following: false,
+    };
+
+
+    setPosts([...posts, newPost]);
+    setContent("");
+    setAuthor("");
+    setTitle("");
+  }
+
+  return (
+    <div>
+      <div>
+        <input
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          placeholder="Title"
+        />
+        <input
+          type="text"
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+          placeholder="Content"
+        />
+        <input
+          type="text"
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+          placeholder="Author"
+        />
+        <button onClick={addPost}>Add Post</button>
+      </div>
+      <div className="posts">
+        {posts.map((post, idx) => (
+          <Post {...post} key={idx} />
+        ))}
+      </div>
+    </div>
+  );
 }
+
